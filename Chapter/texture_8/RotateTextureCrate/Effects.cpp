89f239f4751f1c90402426ae04bd97f88df0c16f
xxl534 <xiexuelong534@163.com>
2018-10-17 00:02:09 +0800

    first commit

---
+++ Chapter/texture_8/RotateTextureCrate/Effects.cpp
@@ -0,0 +1,73 @@
+#include "Effects.h"
+#include <d3dcompiler.h>
+#pragma region Effect
+Effect::Effect(ID3D11Device* device, const std::wstring& filename)
+	:mFX(NULL)
+{
+	DWORD shaderFlags = 0;
+#if defined( DEBUG ) || defined( _DEBUG )
+	shaderFlags |= D3D10_SHADER_DEBUG;
+	shaderFlags |= D3D10_SHADER_SKIP_OPTIMIZATION;
+#endif
+	ID3D10Blob* compiledShader = NULL;
+	ID3D10Blob* compilationMsgs = NULL;
+	HRESULT hr = D3DCompileFromFile(filename.c_str(), 0, D3D_COMPILE_STANDARD_FILE_INCLUDE, 0, "fx_5_0", shaderFlags, 0, &compiledShader, &compilationMsgs);
+	if (compilationMsgs != NULL)
+	{
+		MessageBoxA(0, (char*)compilationMsgs->GetBufferPointer(), 0, 0);
+		ReleaseCOM(compilationMsgs);
+	}
+
+	HR(D3DX11CreateEffectFromMemory(compiledShader->GetBufferPointer(), compiledShader->GetBufferSize(), 0, device, &mFX));
+}
+
+Effect::~Effect()
+{
+	ReleaseCOM(mFX);
+}
+#pragma endregion
+
+#pragma region BisicEffect
+BasicEffect::BasicEffect(ID3D11Device* device, const std::wstring& filename)
+	:Effect(device, filename)
+{
+	Light1Tech = mFX->GetTechniqueByName("Light1");
+	Light2Tech = mFX->GetTechniqueByName("Light2");
+	Light3Tech = mFX->GetTechniqueByName("Light3");
+
+	Light0TexTech = mFX->GetTechniqueByName("Light0Tex");
+	Light1TexTech = mFX->GetTechniqueByName("Light1Tex");
+	Light2TexTech = mFX->GetTechniqueByName("Light2Tex");
+	Light3TexTech = mFX->GetTechniqueByName("Light3Tex");
+
+	WorldViewProj = mFX->GetVariableByName("gWorldViewProj")->AsMatrix();
+	World = mFX->GetVariableByName("gWorld")->AsMatrix();
+	WorldInvTranspose = mFX->GetVariableByName("gWorldInvTranspose")->AsMatrix();
+	DiffuseTexTransform = mFX->GetVariableByName("gDiffuseTexTransform")->AsMatrix();
+	AlphaTexTransform = mFX->GetVariableByName("gAlphaTexTransform")->AsMatrix();
+	EyePosW = mFX->GetVariableByName("gEyePosW")->AsVector();
+	DirLights = mFX->GetVariableByName("gDirLights");
+	Mat = mFX->GetVariableByName("gMaterial");
+	DiffuseMap = mFX->GetVariableByName("gDiffuseMap")->AsShaderResource();
+	AlphaMap = mFX->GetVariableByName("gAlphaMap")->AsShaderResource();
+}
+
+BasicEffect::~BasicEffect()
+{
+
+}
+#pragma endregion
+
+#pragma region Effects
+BasicEffect* Effects::BasicFX = NULL;
+void Effects::InitAll(ID3D11Device* device)
+{
+	BasicFX = new BasicEffect(device, L"FX/Basic.fx");
+}
+
+void Effects::DestroyAll()
+{
+	SafeDelete(BasicFX);
+}
+
+#pragma endregion
---
+++ Common/d3dUtil.cpp
@@ -0,0 +1,203 @@
+//***************************************************************************************
+// d3dUtil.cpp by Frank Luna (C) 2011 All Rights Reserved.
+//***************************************************************************************
+
+#include "d3dUtil.h"
+#include <WICTextureLoader.h>
+#include <wincodec.h>
+#include <wrl\client.h>
+
+bool g_WIC2 = false;
+BOOL WINAPI InitializeWICFactory(PINIT_ONCE, PVOID, PVOID* ifactory) noexcept
+{
+#if (_WIN32_WINNT >= _WIN32_WINNT_WIN8) || defined(_WIN7_PLATFORM_UPDATE)
+	HRESULT hr = CoCreateInstance(
+		CLSID_WICImagingFactory2,
+		nullptr,
+		CLSCTX_INPROC_SERVER,
+		__uuidof(IWICImagingFactory2),
+		ifactory
+	);
+
+	if (SUCCEEDED(hr))
+	{
+		// WIC2 is available on Windows 10, Windows 8.x, and Windows 7 SP1 with KB 2670838 installed
+		g_WIC2 = true;
+		return TRUE;
+	}
+	else
+	{
+		hr = CoCreateInstance(
+			CLSID_WICImagingFactory1,
+			nullptr,
+			CLSCTX_INPROC_SERVER,
+			__uuidof(IWICImagingFactory),
+			ifactory
+		);
+		return SUCCEEDED(hr) ? TRUE : FALSE;
+	}
+#else
+	return SUCCEEDED(CoCreateInstance(
+		CLSID_WICImagingFactory,
+		nullptr,
+		CLSCTX_INPROC_SERVER,
+		__uuidof(IWICImagingFactory),
+		ifactory)) ? TRUE : FALSE;
+#endif
+}
+IWICImagingFactory* _GetWIC()
+{
+	static INIT_ONCE s_initOnce = INIT_ONCE_STATIC_INIT;
+
+	IWICImagingFactory* factory = nullptr;
+	InitOnceExecuteOnce(&s_initOnce, InitializeWICFactory, nullptr, reinterpret_cast<LPVOID*>(&factory));
+	return  factory;
+}
+
+ID3D11ShaderResourceView* d3dHelper::CreateRandomTexture1DSRV(ID3D11Device* device)
+{
+	// 
+	// Create the random data.
+	//
+	XMFLOAT4 randomValues[1024];
+
+	for(int i = 0; i < 1024; ++i)
+	{
+		randomValues[i].x = MathHelper::RandF(-1.0f, 1.0f);
+		randomValues[i].y = MathHelper::RandF(-1.0f, 1.0f);
+		randomValues[i].z = MathHelper::RandF(-1.0f, 1.0f);
+		randomValues[i].w = MathHelper::RandF(-1.0f, 1.0f);
+	}
+
+    D3D11_SUBRESOURCE_DATA initData;
+    initData.pSysMem = randomValues;
+	initData.SysMemPitch = 1024*sizeof(XMFLOAT4);
+    initData.SysMemSlicePitch = 0;
+
+	//
+	// Create the texture.
+	//
+    D3D11_TEXTURE1D_DESC texDesc;
+    texDesc.Width = 1024;
+    texDesc.MipLevels = 1;
+    texDesc.Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+    texDesc.Usage = D3D11_USAGE_IMMUTABLE;
+    texDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
+    texDesc.CPUAccessFlags = 0;
+    texDesc.MiscFlags = 0;
+    texDesc.ArraySize = 1;
+
+	ID3D11Texture1D* randomTex = 0;
+    HR(device->CreateTexture1D(&texDesc, &initData, &randomTex));
+
+	//
+	// Create the resource view.
+	//
+    D3D11_SHADER_RESOURCE_VIEW_DESC viewDesc;
+	viewDesc.Format = texDesc.Format;
+    viewDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE1D;
+    viewDesc.Texture1D.MipLevels = texDesc.MipLevels;
+	viewDesc.Texture1D.MostDetailedMip = 0;
+	
+	ID3D11ShaderResourceView* randomTexSRV = 0;
+    HR(device->CreateShaderResourceView(randomTex, &viewDesc, &randomTexSRV));
+
+	ReleaseCOM(randomTex);
+
+	return randomTexSRV;
+}
+
+void 
+DXErrorMessage(HRESULT hr)
+{
+	static WCHAR buffer[256];
+	FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM, NULL, hr, 0, buffer, 256, NULL);
+	wprintf(buffer);
+}
+
+template<int col> inline  
+float MatrixCom(CXMMATRIX m, int row)
+{
+	return 0.f;
+}
+template<> inline
+float MatrixCom<0>(CXMMATRIX m, int row)
+{
+	return ::XMVectorGetX(m.r[row]);
+}
+template<> inline
+float MatrixCom<1>(CXMMATRIX m, int row)
+{
+	return ::XMVectorGetY(m.r[row]);
+}
+template<> inline
+float MatrixCom<2>(CXMMATRIX m, int row)
+{
+	return ::XMVectorGetZ(m.r[row]);
+}
+template<> inline
+float MatrixCom<3>(CXMMATRIX m, int row)
+{
+	return ::XMVectorGetW(m.r[row]);
+}
+
+void ExtractFrustumPlanes(XMFLOAT4 planes[6], CXMMATRIX M)
+{
+	//
+	// Left
+	//
+#define Matrix4Com( row, col ) (MatrixCom<col>(M,row) )
+
+
+	planes[0].x = Matrix4Com(0,3) + Matrix4Com(0,0);
+	planes[0].y = Matrix4Com(1,3) + Matrix4Com(1,0);
+	planes[0].z = Matrix4Com(2,3) + Matrix4Com(2,0);
+	planes[0].w = Matrix4Com(3,3) + Matrix4Com(3,0);
+
+	//
+	// Right
+	//
+	planes[1].x = Matrix4Com(0,3) - Matrix4Com(0,0);
+	planes[1].y = Matrix4Com(1,3) - Matrix4Com(1,0);
+	planes[1].z = Matrix4Com(2,3) - Matrix4Com(2,0);
+	planes[1].w = Matrix4Com(3,3) - Matrix4Com(3,0);
+
+	//
+	// Bottom
+	//
+	planes[2].x = Matrix4Com(0,3) + Matrix4Com(0,1);
+	planes[2].y = Matrix4Com(1,3) + Matrix4Com(1,1);
+	planes[2].z = Matrix4Com(2,3) + Matrix4Com(2,1);
+	planes[2].w = Matrix4Com(3,3) + Matrix4Com(3,1);
+
+	//
+	// Top
+	//
+	planes[3].x = Matrix4Com(0,3) - Matrix4Com(0,1);
+	planes[3].y = Matrix4Com(1,3) - Matrix4Com(1,1);
+	planes[3].z = Matrix4Com(2,3) - Matrix4Com(2,1);
+	planes[3].w = Matrix4Com(3,3) - Matrix4Com(3,1);
+
+	//
+	// Near
+	//
+	planes[4].x = Matrix4Com(0,2);
+	planes[4].y = Matrix4Com(1,2);
+	planes[4].z = Matrix4Com(2,2);
+	planes[4].w = Matrix4Com(3,2);
+
+	//
+	// Far
+	//
+	planes[5].x = Matrix4Com(0,3) - Matrix4Com(0,2);
+	planes[5].y = Matrix4Com(1,3) - Matrix4Com(1,2);
+	planes[5].z = Matrix4Com(2,3) - Matrix4Com(2,2);
+	planes[5].w = Matrix4Com(3,3) - Matrix4Com(3,2);
+#undef Matrix4Com
+	// Normalize the plane equations.
+	for(int i = 0; i < 6; ++i)
+	{
+		XMVECTOR v = XMPlaneNormalize(XMLoadFloat4(&planes[i]));
+		XMStoreFloat4(&planes[i], v);
+	}
+}
